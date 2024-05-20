5f11e4e55a0199d26eed2c4fe2f28c8a55473b24
Harsh Shukla <devloper.hs2015@gmail.com>
2024-05-20 14:59:57 +0000

    changed code for qn13.py

--- Variables , Data Types, Strings/qn13.py
+++ Variables , Data Types, Strings/qn13.py
@@ -1,17 +1,18 @@
-# Accept a positive integer x as input, compute the value of this continued fraction and store 
+# Accept a positive integer x as input, compute the value of  continued fraction and store 
 # the result in a variable named cfrac and print the result 
 
 # x -> input -> continued fraction -> cfrac -> print
 
-# user input
+
+# user _input
 x = int(input())
 
-#setup
+# setup
 x1 = x + (1/x)
 x2 = x + (1/x1)
 x3 = x + (1/x2)
-x4 = x + (1/x3)
-x5 = x + (1/x4)
-cfrac = x + (1/x5)
+x4 = x+ (1/x3)
+cfrac = x+(1/x4)
 
+# print
 print(round(cfrac,2))
