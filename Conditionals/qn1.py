b9fcd53fce6321f838ea17e72a67423a75aec380
Harsh Shukla <devloper.hs2015@gmail.com>
2024-05-30 08:13:48 +0000

    added qn1

--- Conditionals/qn1.py
+++ Conditionals/qn1.py
@@ -1,1 +1,10 @@
 # Accept a non-zero integer as input. Print positive if it is greater than zero and negative if it is less than zero.
+
+x = int(input())
+
+if x>0:
+    print("positive")
+elif x<0:
+    print("negative")
+else:
+    print("invalid")
