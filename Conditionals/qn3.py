e1087778ce1570d9dd9851254519a7f2b7b88aa4
Harsh Shukla <devloper.hs2015@gmail.com>
2024-05-30 08:29:52 +0000

    added shift clock code

--- Conditionals/qn3.py
+++ Conditionals/qn3.py
@@ -6,3 +6,16 @@ Input: single line containing an integer
 Output: single line containing "Output" String
 '''
 
+# user_input
+t = int(input())
+
+if 0 <= t <= 5:
+    print("NIGHT")
+elif 6<= t <= 11:
+    print("MORNING")
+elif 12 <= t <= 17:
+    print("AFTERNOON")
+elif 18 <= t <= 23:
+    print("EVENING")
+else:
+    print("INVALID")
