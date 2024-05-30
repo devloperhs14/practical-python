d91c26c9c13178ad8d96382ef690cf81318eebc8
Harsh Shukla <devloper.hs2015@gmail.com>
2024-05-30 09:01:10 +0000

    added quadrant checker file

--- Conditionals/qn4.py
+++ Conditionals/qn4.py
@@ -1,3 +1,4 @@
+# 2D Quadrant Checker
 '''
 Accept a point in 2D space as input and find the region in space that this point belongs to. 
 A point could belong to one of the four quadrants (I, II , III, IV), 
@@ -9,4 +10,21 @@ Input: x, y (seprate lines)
 Output: first, second, third, fourth, x-axis, y-axis, and origin
 '''
 
-import time
+# user input
+x = float(input())
+y = float(input())
+
+if x==0 and y==0:
+    print("origin")
+elif y == 0 and x!=0:
+    print("x-axis")
+elif y!=0 and x==0:
+    print("y-axis")
+elif x>0 and y>0:
+    print("first")
+elif x<0 and y>0:
+    print("second")
+elif x<0 and y<0:
+    print("third")
+elif x>0 and y<0:
+    print("fourth")
