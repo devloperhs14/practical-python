42d42835d92ea514a7e11b88973aa88be24ade17
Harsh Shukla <devloper.hs2015@gmail.com>
2024-05-30 08:21:25 +0000

    added peice wise fn code

--- Conditionals/qn2.py
+++ Conditionals/qn2.py
@@ -6,3 +6,12 @@ Condtions:
 * both the input x and output f(x) should be real numbers / float
 """
 
+#user input
+x = float(input())
+
+if 0<x<10:
+    print(x+2)
+elif 10<=x:
+    print((x**2)+2)
+else:
+    print(0)
