96e402b67082882f3ca6c02e7f2eb8728dcf92a0
Harsh Shukla <devloper.hs2015@gmail.com>
2024-06-03 15:52:19 +0000

    qn for qn7.py

---
+++ Conditionals/qn7.py
@@ -0,0 +1,12 @@
+'''
+check_magical_words
+Write a Python program that ]
+    accepts a sequence of five words as input, one word on each line, 
+    prints "magical" if the sequence is magical and "non-magical" otherwise.
+
+magical : A word str1 is a substring of another word str2 if and only if str1 is present as a sequence of consecutive characters in str2.
+
+Eg:
+ab -> abc -> abcd -> abcde = magical
+abk -> abc -> abcd -> abcde = non magical
+'''
