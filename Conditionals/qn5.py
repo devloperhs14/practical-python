16b6c623c389e256f2dc25f7b416a96de8fe62e5
Harsh Shukla <devloper.hs2015@gmail.com>
2024-06-03 15:44:16 +0000

    added code for qn5.py and qn6.py

--- Conditionals/qn5.py
+++ Conditionals/qn5.py
@@ -1,3 +1,4 @@
+
 '''
 Write a program to realize the equation of a line given 2 points (x1,y1) and (x2,y2) in 2D space.
 Hint - see qn5.py to calculate.
@@ -13,3 +14,35 @@ if line vertical :
 if not:
     y3 = Positive , Negative , Zero
 '''
+
+
+# user_input
+x1 , y1 = float(input()), float(input()) # 1st point
+x2 , y2 = float(input()), float(input()) # 2nd point
+x3 = float(input())
+#y3?
+
+
+# vertical line x1!=x2
+
+if x1!=x2:
+    #calc slope
+    m = (y2-y1)/(x2-x1) 
+
+    #y3 = m(x3-x1)+y1
+    y3 = m*(x3-x1)+y1
+    print(y3)
+
+    # slope type
+    if m ==0:
+        print("Horizontal Line")
+    elif m>0:
+        print("Positive Slope")
+    else:
+        print("Negative Slope")
+
+else:
+    print("Vertical Line")
+
+
+
---
+++ Conditionals/qn6.py
@@ -0,0 +1,18 @@
+'''
+Equal Substring Finder!
+
+Given an input string:
+
+If it's odd in length, keep it.
+If it's even:
+    Remove the last character if it's a period.
+    Add a period if the last character isn't a period.
+
+Then: 
+    Find a substring of three consecutive characters from the modified string.
+    Ensure there's an equal number of characters to the left and right of this substring.
+    Print this substring.
+
+    e.g: example. -> example -> amp
+'''
+
